29ec6ffccd7e6005835c518d626de7c6994331d0
Jimmy <odylic.lin@gmail.com>
2021-06-08 21:09:37 -0400

    added youtube router and youtube controller

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 // server/index.js
 const express = require("express");
 const path = require('path');
+import youtuberouter from './routers/youtuberouter'
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Route requests for the user endpoints
+app.use('/api/youtube', youtuberouter)
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server Test!" });
@@ -15,3 +19,5 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
